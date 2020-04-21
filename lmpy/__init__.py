2c809ba15da81405d28f795516090ae1462a049b
CJ Grady <cjgrady@gmail.com>
2020-04-21 15:21:51 -0500

    Changed from namedtuple to class
    Necessary for support of python versions before 3.7

--- lmpy/__init__.py
+++ lmpy/__init__.py
@@ -5,9 +5,7 @@ from . import randomize
 from . import tree
 from .matrix import *
 from .tree import *
-from collections import namedtuple
-
-Point = namedtuple('Point', 'species_name, x, y, flags', defaults=[None])
+from .point import Point
 
 __all__ = ['randomize']
 __all__.extend(matrix.__all__)
