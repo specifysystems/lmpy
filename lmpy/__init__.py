2c5324855f15ff519c3a09b5bd238f34b29f1770
CJ Grady <cjgrady@gmail.com>
2020-04-21 15:07:53 -0500

    Tests for occurrence filters

--- lmpy/__init__.py
+++ lmpy/__init__.py
@@ -5,7 +5,11 @@ from . import randomize
 from . import tree
 from .matrix import *
 from .tree import *
+from collections import namedtuple
+
+Point = namedtuple('Point', 'species_name, x, y, flags', defaults=[None])
 
 __all__ = ['randomize']
 __all__.extend(matrix.__all__)
 __all__.extend(tree.__all__)
+__all__.append('Point')
