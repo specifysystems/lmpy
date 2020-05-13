63cbc38cfd80b71dc4dda61a593c6f469858a041
CJ Grady <cjgrady@gmail.com>
2020-05-13 12:41:35 -0500

    First commit of coordinate conversion

--- tests/test_data_preparation/test_occurrence_transformation.py
+++ tests/test_data_preparation/test_occurrence_transformation.py
@@ -9,6 +9,8 @@ from lmpy import Point
 from lmpy.data_preparation.occurrence_transformation import (
     convert_delimited_to_point, convert_json_to_point, none_getter)
 from _operator import itemgetter
+from data_preparation.occurrence_transformation import get_coordinate_converter
+from copy import deepcopy
 
 
 # .............................................................................
@@ -327,3 +329,50 @@ class Test_convert_json_to_point:
             json_obj, itemgetter('species'), itemgetter('x'), itemgetter('y'),
             point_iterator=itemgetter('points'))
         self._check_points(points)
+
+
+# .............................................................................
+class Test_get_coordinate_converter:
+    """Test get_coordinate_converter."""
+    # ..........................
+    def test_specific_values(self):
+        """Test that conversion produces known values."""
+        in_vals = (4326, -89.555857, 37.3040553)
+        out_vals = (2815, 333699.36, 163612.51)
+        converter = get_coordinate_converter(in_vals[0], out_vals[0])
+        in_point = Point('test species', in_vals[1], in_vals[2])
+        out_point = converter(in_point)
+        assert np.isclose(out_point.x, out_vals[1])
+        assert np.isclose(out_point.y, out_vals[2])
+        assert in_point.species_name == out_point.species_name
+
+    # ..........................
+    def test_to_and_back(self):
+        """Test conversion from one to another and back."""
+        in_vals = (4326, -89.555857, 37.3040553)
+        converter = get_coordinate_converter(in_vals[0], 3395)
+        converter_2 = get_coordinate_converter(3395, in_vals[0])
+        in_point = Point('test species', in_vals[1], in_vals[2])
+        out_point = converter(in_point)
+        out_point_2 = converter_2(out_point)
+        assert np.isclose(out_point_2.x, in_vals[1])
+        assert np.isclose(out_point_2.y, in_vals[2])
+        assert in_point.species_name == out_point.species_name
+
+    # ..........................
+    def test_transitive(self):
+        """Test conversion from one to another to another and back."""
+        in_vals = (4326, -89.555857, 37.3040553)
+        epsgs = [3395, 2815]
+        original_point = Point('Test species', in_vals[1], in_vals[2])
+        in_point = deepcopy(original_point)
+        src_epsg = in_vals[0]
+        for target_epsg in epsgs:
+            converter = get_coordinate_converter(src_epsg, target_epsg)
+            in_point = converter(in_point)
+            src_epsg = target_epsg
+        final_converter = get_coordinate_converter(src_epsg, in_vals[0])
+        final_point = final_converter(in_point)
+        assert np.isclose(final_point.x, original_point.x)
+        assert np.isclose(final_point.y, original_point.y)
+        assert final_point.species_name == original_point.species_name
