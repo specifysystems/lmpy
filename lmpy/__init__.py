82af57d8a4c46c950367882f4c29039f1aac3b9b
CJ Grady <cjgrady@gmail.com>
2020-05-15 09:42:08 -0500

    Fix style issues

--- lmpy/__init__.py
+++ lmpy/__init__.py
@@ -8,17 +8,8 @@ from .tree import *
 from .point import Point
 from . import data_preparation
 from . import statistics
-#from .data_preparation import data_preparation
-#from .randomize import randomize
-#from .statistics import statistics
 
 __all__ = ['data_preparation', 'randomize', 'statistics']
 __all__.extend(matrix.__all__)
 __all__.extend(tree.__all__)
 __all__.append('Point')
-#__all__.extend(data_preparation)
-#__all__.extend(randomize)
-#__all__.extend(statistics)
-#__all__.append('data_preparation')
-#__all__.append('randomize')
-#__all__.append('statistics')
