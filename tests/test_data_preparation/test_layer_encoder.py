7bbf0783c76444e9f9af4296cc768939da6358e3
CJ Grady <cjgrady@gmail.com>
2020-11-30 14:09:15 -0600

    Add more tests for min coverage

--- tests/test_data_preparation/test_layer_encoder.py
+++ tests/test_data_preparation/test_layer_encoder.py
@@ -1,9 +1,13 @@
 """Tests the occurrence data filters
 """
+import glob
 import json
+import os
+import tempfile
 
 import numpy as np
 
+from lmpy.data_preparation.build_grid import build_shapegrid
 from lmpy.data_preparation.layer_encoder import LayerEncoder, DEFAULT_NODATA
 
 
@@ -119,3 +123,60 @@ class Test_LayerEncoder:
         assert np.nanmin(enc_mtx) >= DEFAULT_NODATA
         _ = enc_mtx[enc_mtx > DEFAULT_NODATA]
         assert json.loads(json.dumps(encoder.get_geojson()))
+
+    # ................................
+    def test_min_coverage_encode_presence_absence(self):
+        """Test encode presence absence minimum coverage parameter."""
+        # Create shapegrid (10 x 10 1 degree cells)
+        t_file = tempfile.NamedTemporaryFile(delete=True)
+        base_filename = t_file.name
+        t_file.close()
+
+        shapegrid_filename = '{}.shp'.format(base_filename)
+        layer_filename = '{}.asc'.format(base_filename)
+
+        build_shapegrid(shapegrid_filename, 0, 0, 10, 10, 1, 4326, 4)
+
+        encoder = LayerEncoder(shapegrid_filename)
+
+        # Create layer
+        with open(layer_filename, mode='wt') as layer_out:
+            layer_out.write('ncols    40\n')
+            layer_out.write('nrows    40\n')
+            layer_out.write('xllcorner    0\n')
+            layer_out.write('yllcorner    0\n')
+            layer_out.write('cellsize    0.25\n')
+            for i in range(40):
+                vals = ['0'] * 40
+                j = i
+                while j < 40:
+                    vals[j] = '5'
+                    j += 1
+                layer_out.write('{}\n'.format(' '.join(vals)))
+                print(' '.join(vals))
+
+        # Encode layer with multiple coverages
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 1 percent', 1, 100, 1)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 2 percent', 1, 100, 2)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 10 percent', 1, 100, 10)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 40 percent', 1, 100, 40)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 70 percent', 1, 100, 70)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 90 percent', 1, 100, 90)
+        encoder.encode_presence_absence(
+            layer_filename, 'Layer 100 percent', 1, 100, 100)
+
+        enc_matrix = encoder.get_encoded_matrix()
+
+        # Delete temp files
+        for fn in glob.glob('{}.*'.format(base_filename)):
+            os.remove(fn)
+
+        # Check encoding
+        assert np.all(
+            enc_matrix.sum(axis=0) == np.array([55, 55, 55, 55, 45, 45, 45]))
