8bbfbcb30b7b2bbfb202601e04e659c559207925
CJ Grady <cjgrady@gmail.com>
2022-01-21 11:35:53 -0600

    No qa for import after path manipulation

--- _sphinx_config/conf.py
+++ _sphinx_config/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 
-import lmpy
-
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+
+import lmpy  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 project = 'Specify lmpy'
