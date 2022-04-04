bedb34ba5f1ee5435651043c58adf8c7d268394d
CJ Grady <cjgrady@gmail.com>
2022-04-04 11:06:44 -0500

    Fill in matrix data wrangler base class

--- lmpy/data_wrangling/matrix/base.py
+++ lmpy/data_wrangling/matrix/base.py
@@ -1,4 +1,6 @@
 """Module containing Matrix Data Wrangler base class."""
+import numpy as np
+
 from lmpy.data_wrangling.base import _DataWrangler
 
 
@@ -8,6 +10,63 @@ class _MatrixDataWrangler(_DataWrangler):
     name = '_MatrixDataWrangler'
 
     # .......................
-    def __init__(self):
-        """Constructor for _MatrixDataWrangler base class."""
-        pass
+    def __init__(self, **params):
+        """Constructor for _MatrixDataWrangler base class.
+
+        Args:
+            **params (dict): Named parameters to pass to _DataWrangler base class.
+        """
+        _DataWrangler(self, **params)
+
+    # .......................
+    def wrangle_matrix(self, matrix):
+        """Wrangle a matrix.
+
+        Args:
+            matrix (Matrix): A matrix to wrangle.
+
+        Raises:
+            NotImplementedError: This method is not implemeneted for the base class.
+        """
+        raise NotImplementedError('wrangle_matrix not implemented on base class.')
+
+    # .......................
+    def wrangle_axis(self, matrix, axis, wrangle_func, purge_failures=True):
+        """Wrangle a matrix axis.
+
+        Args:
+            matrix (Matrix): The matrix to wrangle.
+            axis (int): The axis to wrangle.
+            wrangle_func (Method): A function that takes a header and a matrix slice
+                and returns a boolean.
+            purge_failures (bool): Should failures be purged from the matrix.
+
+        Returns:
+            Matrix: A wrangled matrix.
+        """
+        headers = matrix.get_headers(axis=axis)
+        failures = []
+
+        for i, vals in enumerate(np.rollaxis(axis)):
+            if not wrangle_func(headers[i], vals):
+                failures.append(i)
+
+        # Handle failures
+        if purge_failures:
+            # Purge failures
+            matrix = np.delete(matrix, failures, axis=axis)
+        return matrix
+
+    # .......................
+    def reorder(self, matrix, *axes_order):
+        """Reorder / subset a matrix for a number of dimensions.
+
+        Args:
+            matrix (Matrix): A matrix to reorder.
+            *axes_order (list of iterables): Iterables for each dimension of a matrix
+                to reorder / subset.
+
+        Returns:
+            Matrix: A reordered and / or subset matrix.
+        """
+        return matrix.slice(*axes_order)
