da41402034446e4739bdfd2bc53a85ab51283de5
CJ Grady <cjgrady@gmail.com>
2022-04-05 17:03:51 -0500

    Add tests for wrangle_tree tool and fixes

--- lmpy/data_wrangling/tree/__init__.py
+++ lmpy/data_wrangling/tree/__init__.py
@@ -1,2 +1,13 @@
 """Tree data wranglers."""
-__all__ = []
+
+from . import accepted_name_wrangler
+from . import base
+from . import match_matrix_wrangler
+from . import subset_tree_wrangler
+
+__all__ = [
+    'accepted_name_wrangler',
+    'base',
+    'match_matrix_wrangler',
+    'subset_tree_wrangler',
+]
