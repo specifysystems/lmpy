3093acb9db8aea3416696c2261b1479a0d698263
GitHub Action <action@github.com>
2022-04-22 00:38:30 +0000

    Add changes for 505f3b6184903dc010b952f30353fb6f4d039c18

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.1+4.g505f3b6',
+    VERSION: '3.1.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
