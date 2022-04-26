cf2d5e9ae159ee55797aa4064746e0a59ba27607
CJ Grady <cjgrady@gmail.com>
2022-04-26 13:56:24 -0500

    Initial commit of functionality
    Functions to create geojson from matrix with or without shapefile

--- lmpy/spatial/__init__.py
+++ lmpy/spatial/__init__.py
@@ -1,7 +1,10 @@
 """Spatial tools."""
 # flake8: noqa
+from . import geojsonify
 from . import spatial_index
+from .geojsonify import *
 from .spatial_index import *
 
 __all__ = []
+__all__.extend(geojsonify.__all__)
 __all__.extend(spatial_index.__all__)
