3686dcd84eb9162ea520af1eae64ba4d3ab6a13b
CJ Grady <cjgrady@gmail.com>
2022-04-29 12:10:01 -0500

    Add option to provide configuration file to split occurrences

--- lmpy/tools/split_occurrence_data.py
+++ lmpy/tools/split_occurrence_data.py
@@ -19,174 +19,60 @@ species) can be processed together.'''
 
 
 # .....................................................................................
-def get_writer_filename_func(base_dir):
-    """Get a function that returns a filename from a writer key.
+def _process_arguments(parser):
+    """Process arguments including filling in those provided by configuration file.
 
     Args:
-        base_dir (str): A base directory for all writers.
+        parser (argparse.ArgumentParser): An argparse.ArgumentParser with parameters.
 
     Returns:
-        Method: A function that returns a filename when given a writer key.
+        argparse.Namespace: An augmented Namespace with any parameters specified in a
+            configuration file.
     """
+    args = parser.parse_args()
 
-    def get_writer_filename_from_key(writer_key):
-        """Get a writer filename from a writer key and create directories if needed.
+    if args.config_file is not None:
+        with open(args.config_file, mode='rt') as in_json:
+            config = json.load(in_json)
+            if 'csv' in config.keys():
+                if not isinstance(config['csv'], list):
+                    config['csv'] = [config['csv']]
+                if args.csv is None:
+                    args.csv = []
+                for csv_dict in config['csv']:
+                    args.csv.append(
+                        [
+                            csv_dict['csv_filename'],
+                            csv_dict['wrangler_config_filename'],
+                            csv_dict['species_key'],
+                            csv_dict['x_key'],
+                            csv_dict['y_key'],
+                        ]
+                    )
+            if 'dwca' in config.keys():
+                if not isinstance(config['dwca'], list):
+                    config['dwca'] = [config['dwca']]
+                if args.dwca is None:
+                    args.dwca = []
+                for dwca_dict in config['dwca']:
+                    args.dwca.append(
+                        [
+                            dwca_dict['dwca_filename'],
+                            dwca_dict['wrangler_config_filename'],
+                        ]
+                    )
 
-        Args:
-            writer_key (str or list): A writer key to use to generate a filename.
-
-        Returns:
-            str: A file path to use for the writer.
-        """
-        try:
-            writer_fn = '{}.csv'.format(os.path.join(base_dir, writer_key))
-        except TypeError:  # Tried to join iterable
-            writer_fn = '{}.csv'.format(os.path.join(base_dir, *writer_key))
-        os.makedirs(os.path.dirname(writer_fn), exist_ok=True)
-        return writer_fn
-
-    return get_writer_filename_from_key
-
-
-# .....................................................................................
-def get_writer_key_from_fields_func(*fields):
-    """Get a function that returns a writer key from fields of a point.
-
-    Args:
-        *fields (list): A list of fields to use to determine the Point's writer key.
-
-    Returns:
-        Method: A function that takes a Point as an argument and returns a key.
-    """
-    key_fields = list(fields)
-
-    # .......................
-    def key_from_fields_func(point):
-        """Get a wrangler key for a point.
-
-        Args:
-            point (Point): A point object to get a writer key for.
-
-        Returns:
-            Object: An object representing the key for the particular point.
-        """
-        writer_key = [point.get_attribute(fld) for fld in key_fields]
-        if len(writer_key) == 1:
-            return writer_key[0]
-        return writer_key
-
-    return key_from_fields_func
-
-
-# .....................................................................................
-class OccurrenceSplitter:
-    """A tool for splitting occurrence data by some criteria for easier processing."""
-
-    # .......................
-    def __init__(
-        self,
-        writer_key_func,
-        writer_filename_func,
-        write_fields=None,
-        max_writers=DEFAULT_MAX_WRITERS,
-    ):
-        """Constructor.
-
-        Args:
-            writer_key_func (Method): A function for determining a writer to use.  It
-                should take a Point as input and return a dictionary key.
-            writer_filename_func (Method): A function to determine the file location
-                to write data for a particular writer.  It should take a dictionary
-                key and return a string.
-            write_fields (list): A list of fields to write for each writer.  If None,
-                use all fields in the first output Point object.
-            max_writers (int): The maximum number of open writers (files) at any given
-                time.
-        """
-        self.get_writer_key = writer_key_func
-        self.get_writer_filename = writer_filename_func
-        self.writer_fields = write_fields
-        self.max_writers = max_writers
-        self.writers = {}
-
-    # .......................
-    def __enter__(self):
-        """Context manager magic method.
-
-        Returns:
-            OccurrenceSplitter: This instance.
-        """
-        return self
-
-    # .......................
-    def __exit__(self, *args):
-        """Context manager magic method on exit.
-
-        Args:
-            *args: Position arguments passed to the method.
-        """
-        self.flush_writers()
-
-    # .......................
-    def flush_writers(self):
-        """Close all open occurrence writers."""
-        for writer in self.writers:
-            writer.close()
-        self.writers = {}
-
-    # .......................
-    def open_writer(self, writer_key):
-        """Open an occurrence writer for the provided key.
-
-        Args:
-            writer_key (object): Some key that can be used to determine a writer.
-        """
-        # Flush or close writers if needed
-        if len(self.writers) >= self.max_writers:
-            self.flush_writers
-        # Open the new writer
-        self.writers[writer_key] = PointCsvWriter(
-            self.get_writer_filename(writer_key), self.writer_fields
-        )
-        self.writers[writer_key].open()
-
-    # .......................
-    def process_reader(self, reader, wranglers):
-        """Process an occurrence reader.
-
-        Args:
-            reader (PointCsvReader or PointDwcaReader): An occurrence reader instance.
-            wranglers (list): A list of occurrence data wranglers.
-        """
-        reader.open()
-        for points in reader:
-            for wrangler in wranglers:
-                if points:
-                    points = wrangler(points)
-            if points:
-                self.write_points(points)
-        reader.close()
-
-    # .......................
-    def write_points(self, points):
-        """Write points using the appropriate writer.
-
-        Args:
-            points (list): A list of point objects to write to file.
-        """
-        if points:
-            writer_key = self.get_writer_key(points[0])
-            if writer_key not in self.writers.keys():
-                if self.writer_fields is None:
-                    self.writer_fields = list(points[0].attributes.keys())
-                self.open_writer(writer_key)
-            self.writers[writer_key].write_points(points)
-
+    return args
 
 # .....................................................................................
 def cli():
     """Command-line interface for splitting occurrence datasets."""
     parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument(
+        '--config_file',
+        type=str,
+        help='Configuration file containing script arguments.'
+    )
     parser.add_argument(
         '-m',
         '--max_open_writers',
@@ -241,7 +127,7 @@ def cli():
         type=str,
         help='Directory where the output data should be written.',
     )
-    args = parser.parse_args()
+    args = _process_arguments(parser)
 
     # Establish functions for getting writer key and filename
     writer_key_func = get_writer_key_from_fields_func(*tuple(args.key_field))
