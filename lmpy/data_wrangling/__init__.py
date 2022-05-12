53a04f6210d1fd796890c06def0a312e7e491e99
CJ Grady <cjgrady@gmail.com>
2022-05-12 14:36:45 -0500

    Add SpeciesList class (subclass of set)

--- lmpy/data_wrangling/__init__.py
+++ lmpy/data_wrangling/__init__.py
@@ -2,10 +2,12 @@
 from . import common
 from . import matrix
 from . import occurrence
+from . import species_list
 from . import tree
 
 __all__ = []
 __all__.extend(common.__all__)
 __all__.extend(matrix.__all__)
 __all__.extend(occurrence.__all__)
+__all__ .extend(species_list.__all__)
 __all__.extend(tree.__all__)
