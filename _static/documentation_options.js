30ed6acdbfe073fef7ae6dab8ec8806ac8a13831
GitHub Action <action@github.com>
2022-05-18 18:11:51 +0000

    Add changes for be1bffe20cc585c6eb6408a30afb2e6cbfde8730

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.14+371.gbe1bffe2',
+    VERSION: '3.1.15',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
