ac001f9df9ff79f24259785bd5c0fd03e2100ae1
CJ Grady <cjgrady@gmail.com>
2022-05-19 17:21:05 -0500

    Fix exception call

--- lmpy/tools/create_point_heatmap.py
+++ lmpy/tools/create_point_heatmap.py
@@ -105,7 +105,7 @@ def cli():
     """Provide a command-line tool for creating a point heatmap.
 
     Raises:
-        argparse.ArgumentError: Raised if neither a CSV or DWCA is provided.
+        ArgumentError: Raised if neither a CSV or DWCA is provided.
     """
     parser = build_parser()
     args = _process_arguments(parser, config_arg='config_file')
@@ -117,7 +117,9 @@ def cli():
         for csv_fn, sp_key, x_key, y_key in args.csv:
             readers.append(PointCsvReader(csv_fn, sp_key, x_key, y_key))
     if len(readers) == 0:
-        raise argparse.ArgumentError('Must provide at least one CSV and / or DWCA.')
+        raise argparse.ArgumentError(
+            'csv | dwca', 'Must provide at least one CSV and / or DWCA.'
+        )
 
     point_heatmap = create_point_heatmap_matrix(
         readers,
