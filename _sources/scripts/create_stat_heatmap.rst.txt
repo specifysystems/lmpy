581d8513d9296e75a05920799c0ed3d8221d8f61
GitHub Action <action@github.com>
2022-05-20 14:57:45 +0000

    Add changes for 6aeaa8d3652bf48e180c05bb3b80627973994916

---
+++ _sources/scripts/create_stat_heatmap.rst.txt
@@ -0,0 +1,4 @@
+.. sphinx_argparse_cli::
+  :module: lmpy.tools.create_stat_heatmap
+  :func: build_parser
+  :prog: create_stat_heatmap
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.15+2.gcc6e961a',
+    VERSION: '3.1.15+14.g6aeaa8d3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
