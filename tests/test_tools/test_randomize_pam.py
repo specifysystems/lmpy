e750c0a0aff109ce7ebe9f7a2db5d249ebd93723
CJ Grady <cjgrady@gmail.com>
2022-05-23 11:47:25 -0500

    Add tests for multiple random outputs

--- tests/test_tools/test_randomize_pam.py
+++ tests/test_tools/test_randomize_pam.py
@@ -65,6 +65,43 @@ def test_randomize(monkeypatch, observed_pam, generate_temp_filename):
 
 
 # .....................................................................................
+def test_randomize_multiple(monkeypatch, observed_pam, generate_temp_filename):
+    """Test that the randomize PAM tool works as expected with multiple outputs.
+
+    Args:
+        monkeypatch (pytest.Fixture): A pytest fixture for monkeypatching.
+        observed_pam (Matrix): A Presence-Absence Matrix for testing.
+        generate_temp_filename (pytest.fixture): Fixture to generate filenames.
+    """
+    # Get input marginal totals before randomization
+    row_totals = observed_pam.sum(axis=0)
+    col_totals = observed_pam.sum(axis=1)
+    total = observed_pam.sum()
+
+    obs_pam_filename = generate_temp_filename(suffix='.lmm')
+    rand_pam_filenames = [
+        generate_temp_filename(suffix='.lmm') for _ in range(np.random.randint(2, 10))
+    ]
+    # Write test PAM
+    observed_pam.save(obs_pam_filename)
+
+    # Run tool
+    params = ['randomize_pam.py', obs_pam_filename]
+    params.extend(rand_pam_filenames)
+    monkeypatch.setattr('sys.argv', params)
+    cli()
+
+    # Check output PAMs
+    for rand_pam_filename in rand_pam_filenames:
+        random_pam = Matrix.load(rand_pam_filename)
+
+        assert np.all(random_pam.sum(axis=0) == row_totals)
+        assert np.all(random_pam.sum(axis=1) == col_totals)
+        assert np.all(random_pam.sum() == total)
+        assert not np.all(random_pam != observed_pam)
+
+
+# .....................................................................................
 def test_randomize_config_file(monkeypatch, observed_pam, generate_temp_filename):
     """Test that the randomize PAM tool works as expected with a configuration file.
 
@@ -105,3 +142,49 @@ def test_randomize_config_file(monkeypatch, observed_pam, generate_temp_filename
     assert np.all(random_pam.sum(axis=1) == col_totals)
     assert np.all(random_pam.sum() == total)
     assert not np.all(random_pam != observed_pam)
+
+
+# .....................................................................................
+def test_randomize_config_file_many(monkeypatch, observed_pam, generate_temp_filename):
+    """Test that the randomize PAM tool works as expected with a configuration file.
+
+    Args:
+        monkeypatch (pytest.Fixture): A pytest fixture for monkeypatching.
+        observed_pam (Matrix): A Presence-Absence Matrix for testing.
+        generate_temp_filename (pytest.fixture): Fixture to generate filenames.
+    """
+    # Get input marginal totals before randomization
+    row_totals = observed_pam.sum(axis=0)
+    col_totals = observed_pam.sum(axis=1)
+    total = observed_pam.sum()
+
+    obs_pam_filename = generate_temp_filename(suffix='.lmm')
+    rand_pam_filenames = [
+        generate_temp_filename(suffix='.lmm') for _ in range(np.random.randint(2, 10))
+    ]
+    # Write test PAM
+    observed_pam.save(obs_pam_filename)
+
+    # Create configuration file
+    config_fn = generate_temp_filename(suffix='.json')
+    with open(config_fn, mode='wt') as config_out:
+        json.dump(
+            {
+                'input_pam_filename': obs_pam_filename,
+                'output_pam_filename': rand_pam_filenames,
+            },
+            config_out
+        )
+    # Run tool
+    params = ['randomize_pam.py', '--config_file', config_fn]
+    monkeypatch.setattr('sys.argv', params)
+    cli()
+
+    # Check output PAMs
+    for rand_pam_filename in rand_pam_filenames:
+        random_pam = Matrix.load(rand_pam_filename)
+
+        assert np.all(random_pam.sum(axis=0) == row_totals)
+        assert np.all(random_pam.sum(axis=1) == col_totals)
+        assert np.all(random_pam.sum() == total)
+        assert not np.all(random_pam != observed_pam)
