5b780a79307320b069d3c7c7c1948170d0d08658
GitHub Action <action@github.com>
2022-10-03 15:09:14 +0000

    Add changes for 9bc0636cfe745d3d9b637d4d00944ca5edef5376

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.21+78.g23659bf3',
+    VERSION: '3.1.21+103.g9bc0636c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
