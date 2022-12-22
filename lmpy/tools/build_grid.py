e984d4ed27e95b5aaf72b82da689e9d1ebafc056
zzeppozz <zzeppozz@gmail.com>
2022-12-22 16:25:09 -0600

    logging

--- lmpy/tools/build_grid.py
+++ lmpy/tools/build_grid.py
@@ -145,7 +145,7 @@ def cli():
             raise
         except IOError:
             raise
-        logger.log(f"Wrote report file to {args.report_filename}")
+        logger.log(f"Wrote report file to {args.report_filename}", refname=script_name)
 
 
 # .....................................................................................
